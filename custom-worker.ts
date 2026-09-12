0c360f4d1a83c5ac146e3f802326254ea8d94b66
Francisco Terpolilli <frankpablote@mac.com>
2026-09-12 19:47:13 +0100

    Restore ts-expect-error for OpenNext worker import during build

--- custom-worker.ts
+++ custom-worker.ts
@@ -1,3 +1,4 @@
+// @ts-expect-error OpenNext generates this at build time
 import { default as handler } from "./.open-next/worker.js";
 
 import { LobbyStoreDO } from "./src/durable-objects/lobby-store-do";
